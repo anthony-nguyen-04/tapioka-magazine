80a61f516cccc191028e60adbc2ba8324e640a0c
Anthony Nguyen <anthonynguyenlaas@gmail.com>
2024-01-04 01:11:46 -0600

    fixed typos

--- frontend/src/Components/About.tsx
+++ frontend/src/Components/About.tsx
@@ -100,14 +100,14 @@ const About = ({
           {newLine()}
           <Typography style={{ display: "inline-block" }} variant="h6">
             {bold("TAPIOKA MAGAZINE")} is Oklahoma's first publication with a focus on APIDA-MENA (Asian Pacific Islander Desi American - Middle Eastern North African) culture.
-            Starting off as a monthly magazine under the University of Oklahoma's Asian American Student Association in 2022, Tapioka has seen exponential
+            Starting off as a monthly magazine under the University of Oklahoma's Asian American Student Association in 2022, TAPIOKA has seen exponential
             growth as we continue to evolve and innovate.
           </Typography> 
           {newLine()}
           {newLine()}
           <Typography style={{ display: "inline-block" }} variant="h6">
             Through written and creative media, we hope to highlight Oklahoma's APIDA-MENA community and celebrate the rich heritage and history that comes with it.
-            Three times a year, our team comes together to craft a cohesive issue, diving deep into a particular aspect of the our identity.
+            Three times a year, our team comes together to craft a cohesive issue, diving deep into a particular aspect of our identity.
             From {bold("PASSION")} to {bold("TRAUMA")} and {bold("ACCEPTANCE")}, here is our story.
           </Typography>
           {newLine()}
